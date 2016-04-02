046921ce2e9e84182a973076cabb66699c37bbae
Achmad Buhori <Abebaex@users.noreply.github.com>
2016-04-02 12:25:00 +0700

    remove space

--- functions.php
+++ functions.php
@@ -4,13 +4,13 @@
  //widget sidebar
  function custom_widgets_init() {
 	 register_sidebar( array( 'name' => 'Sidebar',
-	 							'id' => 'sidebar-1',
-								'description'   => 'Tambahhkan widget disini.',
-								'before_widget' => '<div class="widget %2$s">',
-								'after_widget'  => '</div>',
-								'before_title'  => '<h3 class="widget-title">',
-								'after_title'   => '</h3>',
-					));
+	 			'id' => 'sidebar-1',
+				'description'   => 'Tambahhkan widget disini.',
+				'before_widget' => '<div class="widget %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+	));
  }
 add_action( 'widgets_init', 'custom_widgets_init' );
 ?>
